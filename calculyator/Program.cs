990585a02c965c3fcd7e1f9ef46974f086416afb
пк <@DESKTOP-BLQLJB6>
2023-01-30 12:35:36 +0300

    Добавьте файлы проекта.

---
+++ calculyator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("выберите действие");
+Console.WriteLine("1-сложение");
+Console.WriteLine("2-вычитание");
+Console.WriteLine("3-умножение");
+Console.WriteLine("4-деление");
+string a = Console.ReadLine();
+Console.WriteLine("введите первое значение");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе значение");
+int numberr = Convert.ToInt32(Console.ReadLine());
+if (a == "1")
+{
+    Console.WriteLine("результат сложения {0} ", number + numberr );
+}
+if (a == "2")
+{
+    Console.WriteLine("результат сложения {0} ", number - numberr);
+}
+if (a == "3")
+{
+    Console.WriteLine("результат сложения {0} ", number * numberr);
+}
+if (a == "4")
+{
+    Console.WriteLine("результат сложения {0} ", number / numberr);
+}
+
+  
+
+
